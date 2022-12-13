6accc877f744f67241eb51ae81639201a77fa9ad
Aleksandr <kossander@bk.ru>
2022-12-14 01:00:42 +0300

    A program has been created that solves the task

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//  РЕШЕНИЕ ЗАДАЧИ
+
+string [] FillingStringArray(int sizeArray)
+{
+    Console.WriteLine($"Enter {sizeArray} string elements of the array:");
+    string [] array = new string[sizeArray];
+    string temp = string.Empty;
+    int sizeNewArray = 0;
+    for(int i = 0; i < sizeArray; i++)
+    {
+        temp = Console.ReadLine();
+        array[i] = temp;
+        if(temp.Length <= 3) sizeNewArray++;
+    }
+
+    string [] newArray = new string[sizeNewArray];
+    for(int i = 0, j = 0; i < sizeArray; i++, j++)
+    {
+        temp = array[i];
+        if(temp.Length <=3) newArray[j] = temp;
+        else j--;
+    }
+    return newArray;
+}
+
+void PrintStringArray(string [] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine("\b\b]");    
+}
+
+Console.WriteLine("Enter the size of the array: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+PrintStringArray(FillingStringArray(sizeArray));
